274f92127f0a01113879b9db5ca6b114dfd0fe57
hirendoshi9901 <54357148+hirendoshi9901@users.noreply.github.com>
2021-12-27 12:40:03 +0530

    Add files via upload

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZzQxN_aO1iwOANpU6mPLx6-vPudzo4yqlA_cORtSMvQ/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
